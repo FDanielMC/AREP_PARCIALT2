ce6719670422dd0d8e24e32bc0c83c2ff86a88cb
FDanielMC <morenillodaniel440@gmail.com>
2024-04-05 15:11:55 -0500

    Lógica full completada y balanceo de instancias

--- src/main/java/org/example/MathServices.java
+++ src/main/java/org/example/MathServices.java
@@ -1,16 +1,22 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
-
 import static spark.Spark.*;
 
+/**
+ * Clase que proporciona servicios de búsqueda lineal y binario.
+ *
+ * @author Daniel Fernando Moreno Cerón
+ * @version 1.0 (02/04/2024)
+ */
 public class MathServices {
 
     private static final String ERROR = "El elemento no está presente";
 
-
+    /**
+     * Método principal de la aplicación.
+     *
+     * @param args Parámetros de la línea de comandos.
+     */
     public static void main(String[] args) {
         //staticFiles.location("/public");
         port(getPort());
@@ -18,22 +24,27 @@ public class MathServices {
             res.type("application/json");
             String[] list = req.queryParams("list").split(",");
             String value = req.queryParams("value");
-            System.out.println("linear: " + value);
             String output = Integer.toString(linear(stringListToIntegerList(list),Integer.parseInt(value)));
-            System.out.println(jsonResponse("linear", req.queryParams("list"), value, output == "-1" ? ERROR : output));
-            return jsonResponse("linear", req.queryParams("list"), value, output == "-1" ? ERROR : output);
+            output = output.equals("-1") ? ERROR : output;
+            System.out.println(jsonResponse("linear", req.queryParams("list"), value, output));
+            return jsonResponse("linear", req.queryParams("list"), value, output);
         });
         get("/binarysearch", (req, res) -> {
             res.type("application/json");
             String[] list = req.queryParams("list").split(",");
             String value = req.queryParams("value");
-            System.out.println("binary: " + value);
             String output = Integer.toString(binary(stringListToIntegerList(list),Integer.parseInt(value)));
-            return jsonResponse("binary", req.queryParams("list"), value, output == "-1" ? ERROR : output);
+            output = output.equals("-1") ? ERROR : output;
+            System.out.println(jsonResponse("binary", req.queryParams("list"), value, output));
+            return jsonResponse("binary", req.queryParams("list"), value, output);
         });
     }
 
-
+    /**
+     * Obtiene el puerto para la aplicación.
+     *
+     * @return El número de puerto.
+     */
     private static int getPort() {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
@@ -41,6 +52,16 @@ public class MathServices {
         return 4568;
     }
 
+    /**
+     * Método que crea una cadena en formato JSON con la operación, ya sea binaria o lineal, la lista en donde se va a
+     * buscar el elemento, el elemento a buscar y el índice del elemento en caso que se encuentre.
+     *
+     * @param operation
+     * @param inputList
+     * @param inputValue
+     * @param output
+     * @return cadena en formato JSON con toda la información de la búsqueda binaria o lineal
+     */
     public static String jsonResponse(String operation, String inputList, String inputValue, String output) {
         return "{" +
                 " \"operation\": \"" + operation + "\"," +
@@ -50,20 +71,41 @@ public class MathServices {
                 "}";
     }
 
+    /**
+     * Método que hace la búsqueda binaria de un entero en una lista de enteros.
+     *
+     * @param list
+     * @param value
+     * @return
+     */
     public static int binary(int[] list, int value) {
         return recursiveBinary(list, value, 0, list.length-1);
     }
 
+    /**
+     * Método que hace la búsqueda binaria de un entero en una lista de enteros.
+     *
+     * @param list
+     * @param value
+     * @param first
+     * @param last
+     * @return
+     */
     private static int recursiveBinary(int[] list, int value, int first, int last) {
-        int result = -1;
+        if(first > last) return -1;
         int middle = first + (last - first) / 2;
-        if (first > last) result =-1; // El elemento no se encontró.
-        if (list[middle] == value) result = middle;
-        else if (list[middle] > value) result = recursiveBinary(list, value, first, middle - 1);
-        else result = recursiveBinary(list, value, middle + 1, last);
-        return result;
+        if (list[middle] == value) return middle;
+        else if (list[middle] > value) return recursiveBinary(list, value, first, middle - 1);
+        else return recursiveBinary(list, value, middle + 1, last);
     }
 
+    /**
+     * Método que hace búsqueda lineal de un entero en una lista de enteros.
+     *
+     * @param list
+     * @param value
+     * @return
+     */
     public static int linear(int[] list, int value) {
         int result = -1;
         for(int i = 0; i < list.length;i++){
@@ -74,6 +116,12 @@ public class MathServices {
         return result;
     }
 
+    /**
+     * Método que convierte una lista de cadenas de texto que contienen enteros a una lista de enteros
+     *
+     * @param list
+     * @return
+     */
     public static int[] stringListToIntegerList(String[] list){
         int[] listInt = new int[list.length];
         for(int i = 0; i < listInt.length; i++){
@@ -81,5 +129,4 @@ public class MathServices {
         }
         return listInt;
     }
-
 }
--- src/main/java/org/example/ServiceProxy.java
+++ src/main/java/org/example/ServiceProxy.java
@@ -8,31 +8,51 @@ import java.net.URL;
 
 import static spark.Spark.*;
 
+/**
+ * Clase que actúa como proxy para acceder a un servicio.
+ *
+ * @author Daniel Fernando Moreno Cerón
+ * @version 1.0 (05/04/2024)
+ */
 public class ServiceProxy {
 
 
-    private static String[] math_services;
+    private static String[] services;
     private static final String USER_AGENT = "Mozilla/5.0";
     private static int instance = 0;
 
+    /**
+     * Método principal de la aplicación.
+     *
+     * @param args Parámetros de la línea de comandos.
+     */
     public static void main(String[] args) {
 
         staticFiles.location("/public");
         port(getPort());
-        math_services = System.getenv("MATH_SERVICES").split(",");
+        System.out.println(System.getenv("SEARCH_SERVICES"));
+        services = System.getenv("SEARCH_SERVICES").split(",");
         get("/linearsearch", (req, res) -> {
             res.type("application/json");
-            String output = invoker(math_services[instance] + "linearsearch?list=" + req.queryParams("list") + "&value=" + req.queryParams("value"));
-            changeInstance();
+            String output = invoker(services[instance] + "linearsearch?list=" + req.queryParams("list") + "&value=" + req.queryParams("value"));
+            System.out.println("Instancia elegida: " + services[instance]);
+            changeServer();
             return output;
         });
         get("/binarysearch", (req, res) -> {
             res.type("application/json");
-            String output = invoker(math_services[instance] + "binarysearch?list=" + req.queryParams("list") + "&value=" + req.queryParams("value"));
-            changeInstance();
+            String output = invoker(services[instance] + "binarysearch?list=" + req.queryParams("list") + "&value=" + req.queryParams("value"));
+            System.out.println("Instancia elegida: " + services[instance]);
+            changeServer();
             return output;
         });
     }
+
+    /**
+     * Obtiene el puerto para la aplicación.
+     *
+     * @return El número de puerto.
+     */
 
     private static int getPort() {
         if (System.getenv("PORT") != null) {
@@ -41,11 +61,22 @@ public class ServiceProxy {
         return 4567;
     }
 
-    private static void changeInstance() {
-        if (instance == math_services.length - 1) instance = 0;
+    /**
+     * Cambia la instancia del servicio a utilizar.
+     */
+    private static void changeServer() {
+        if (instance == services.length - 1) instance = 0;
         else instance++;
     }
 
+
+    /**
+     * Realiza una petición HTTP GET a una URL y devuelve la respuesta.
+     *
+     * @param url La URL a la que realizar la petición.
+     * @return La respuesta de la petición en formato de cadena.
+     * @throws IOException Si ocurre un error al realizar la petición.
+     */
     private static String invoker(String url) throws IOException {
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
